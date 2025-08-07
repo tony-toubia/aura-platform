7610741426a271f1fa91c3c759e69cca122278d2
Tony Toubia <tony2ba@gmail.com>
2025-08-06 19:48:09 -0500

    Simplify root page to fix 500 error

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,16 +1,6 @@
 import PublicHomePage from './(public)/page'
-import Header from '@/components/layout/header'
-import { Footer } from '@/components/layout/footer'
 
 export default function RootPage() {
   // Show public page for unauthenticated users
-  return (
-    <div className="min-h-screen flex flex-col">
-      <Header />
-      <main className="flex-1">
-        <PublicHomePage />
-      </main>
-      <Footer />
-    </div>
-  )
+  return <PublicHomePage />
 }
